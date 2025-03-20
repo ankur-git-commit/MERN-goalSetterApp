af5d64c433efb6e508a191444518b9b96a640261
ankur-git-commit <ankur.kumar.dev@outlook.com>
2025-03-21 01:49:05 +0530

    db connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from "express"
 import dotenv from "dotenv"
+import colors from 'colors'
+import connectDB from "./config/db.js" 
 import { router } from "./routes/goalRoutes.js"
 import { errorHandler } from "./middleware/errorMiddleware.js"
+
+connectDB()
+
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
