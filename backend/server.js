ce30f9ab42aae7ce236edc3584553bc6ac8f0d51
ankur-git-commit <ankur.kumar.dev@outlook.com>
2025-03-24 03:02:42 +0530

    server.js updated for deployment

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,13 @@ import connectDB from "./config/db.js"
 import { goalRouter } from "./routes/goalRoutes.js"
 import { userRouter } from "./routes/userRoutes.js"
 import { errorHandler } from "./middleware/errorMiddleware.js"
+
+import path from "path"
+import { fileURLToPath } from "url"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const PORT = process.env.PORT || 3000
 
 connectDB()
@@ -17,6 +24,18 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use("/api/goals", goalRouter)
 app.use("/api/users", userRouter)
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+    // Set static folder
+    const frontendBuildPath = path.resolve(__dirname, "../frontend/dist")
+    app.use(express.static(frontendBuildPath))
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(frontendBuildPath, "index.html"))
+    })
+}
+
 app.use(errorHandler)
 
 app.listen(PORT, () => console.log(`server is running at ${PORT}`))
