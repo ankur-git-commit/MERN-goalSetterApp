a4aba7e9a12bed0a007fc20725c02caa908520e0
ankur-git-commit <ankur.kumar.dev@outlook.com>
2025-03-24 19:17:48 +0530

    railway deployment setup

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ import { fileURLToPath } from "url"
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 5000
 
 connectDB()
 
